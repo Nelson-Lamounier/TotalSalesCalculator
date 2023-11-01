425791d0353403f63b47e9a2a186826afe3983d8
Nelson-Lamounier <lamounierleao@gmail.com>
2023-11-01 09:18:30 +0000

    Add project files.

---
+++ TotalSalesCalculator/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace TotalSalesCalculator
+{
+    partial class TotalCalcSales
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFile = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFile
+            // 
+            this.btnFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFile.Location = new System.Drawing.Point(74, 32);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(135, 31);
+            this.btnFile.TabIndex = 1;
+            this.btnFile.Text = "File to Calculate";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(51, 82);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(180, 154);
+            this.listBox1.TabIndex = 2;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(104, 263);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(65, 27);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // TotalCalcSales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(288, 316);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnFile);
+            this.Name = "TotalCalcSales";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.TotalCalcSales_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ TotalSalesCalculator/Form1.cs
@@ -0,0 +1,116 @@
+﻿// Create an application that reads the file provided (Sales.txt) into an array,
+// display the array's contents in a ListBox control,
+// and calculates and display the total of the array's values
+
+// 2. Modify the application to display the following.
+// . The average of the values in the array
+// . The largest value in the array
+// . The Smallest value in the array
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TotalSalesCalculator
+{
+    public partial class TotalCalcSales : Form
+    {
+        public TotalCalcSales()
+        {
+            InitializeComponent();
+
+        }
+        // Modularize in order to use the same buttom for exercise 2
+        // Variables for exercise 2 at form level
+        double smallest;
+        double largest;
+        double sum; // sum now is declare at the form level
+
+        private void TotalCalcSales_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            try // Validading if the file exist
+            {
+                // Array to count how many elements are in the .txt file
+                string[] linesAll = File.ReadAllLines("Sales.txt"); // Read all lines in the txt file
+                double[] valueNum = new double[linesAll.Length]; // To specify the Length of the array based on the item element on the txt file
+                int counter = 0; // Counter to count loop through the indexes of each array
+                sum = 0;
+
+                // Initializing the smallest/largest value based on the index of 1 in the file converted to double
+                smallest = Convert.ToDouble(linesAll[0]); 
+                largest = Convert.ToDouble(linesAll[0]); 
+                // Populate ValueNum from linesAll
+                foreach (string line in linesAll)
+                {
+                    // Initializer - Convert the values that is being read from linesAll to double, one element at the time
+                    valueNum[counter] = Convert.ToDouble(line); // Adding values to the array
+                    // Once converted add it to the sum
+                    // Output and display total once the loop is done
+                    listBox1.Items.Add(valueNum[counter]);
+                    Sum(valueNum[counter]);
+                    NumSmall(valueNum[counter]); // Pass values to the function
+                    NumLarge(valueNum[counter]);
+
+                    counter++;
+                }
+
+                listBox1.Items.Add("\nTotal: " + sum.ToString("n")); // Display total
+                listBox1.Items.Add("Average: " + Average(counter).ToString("n")); // Call the function
+                listBox1.Items.Add("Smallest: " + smallest.ToString("n")); // Call the method
+                listBox1.Items.Add("Largest: " + largest.ToString("n")); // Call the method
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // Manipulate the value on the form
+        private void Sum(double valueNum)
+        {
+            sum += valueNum;
+        }
+
+        // Find the smallest
+        private void NumSmall(double valueNum)
+        {
+            if(valueNum < smallest)
+                smallest = valueNum;
+        }
+        // Find the largest
+        private void NumLarge(double valueNum)
+        {
+            if(valueNum > largest) 
+                largest = valueNum;
+        }
+
+        // Average
+        private double Average(int CountNum)
+        {
+            return sum / CountNum;
+        }
+    }
+}
